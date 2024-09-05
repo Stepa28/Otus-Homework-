7c55cc69bc37dcfaea2890b9a3953f5cd4fe320a
Stepan <cntgf.2010@ya.ru>
2024-09-05 13:56:54 +0300

    Дз №6

---
+++ Parallelism/Parallelism/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Parallelism;
+
+var watch = new Stopwatch();
+
+watch.Start();
+
+var space = new SpaceCounter("c:\\Project\\Otus-Homework-\\Parallelism\\Примеры\\");
+
+watch.Stop();
+Console.WriteLine($"Результат: {space.SpaceCount}");
+
+Console.WriteLine("Elapsed time: " + watch.Elapsed);
---
+++ Parallelism/Parallelism/SpaceCounter.cs
@@ -0,0 +1,40 @@
+﻿namespace Parallelism;
+
+public class SpaceCounter
+{
+    private int _spaceCount;
+    
+    public int SpaceCount => _spaceCount;
+
+    public SpaceCounter(string folderPath)
+        : this(new DirectoryInfo(folderPath)){}
+
+    public SpaceCounter(DirectoryInfo directory)
+    {
+        var tasks = new List<Task>();
+        
+        foreach (var file in directory.GetFiles())
+        {
+            if(file.Extension == ".txt")
+                tasks.Add(SpaceCountInTextFile(file.FullName));
+        }
+        
+        Task.WaitAll(tasks.ToArray());
+    }
+
+    private async Task SpaceCountInTextFile(string fileName)
+    {
+        try
+        {
+            using var reader = new StreamReader(fileName);
+            while (await reader.ReadLineAsync() is { } line)
+            {
+                Interlocked.Add(ref _spaceCount, line.Count(c => c == ' '));
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
+        }
+    }
+}
